35df51e31b9b29bcd51ddb38bcd1b815e22afc8d
Sivaram V <sivaram.v@grasko.com>
2026-05-20 11:43:05 +0530

    Use environment variable for PORT with fallback to 5000

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const getData = () => {
   const data = fs.readFileSync("./db.json");
